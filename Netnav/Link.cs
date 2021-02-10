2786f17ca01b57cbee68d00424c7fe5f93f5bc31
Vladislav Ashtaev <vl.ashtaev@gmail.com>
2021-02-10 23:53:04 +0300

    add Link class

---
+++ Netnav/Link.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netnav
+{
+    class Link
+    {
+        public string Number { get; set; }
+        public Uri Uri { get; set; }
+        public int Status { get; set; }
+    }
+}
